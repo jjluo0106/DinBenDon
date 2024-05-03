b06e5c97884ea3e769650ee3305a7df84454338b
azhe66666666666666 <heyhey@aa.com>
2024-05-03 18:47:33 +0800

    每分鐘回報

--- src/main/java/com/heima/testBreath/MyScheduledTask.java
+++ src/main/java/com/heima/testBreath/MyScheduledTask.java
@@ -1,16 +1,16 @@
-//package com.heima.testBreath;
-//
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//    @Component
-//    @EnableScheduling
-//    public class MyScheduledTask {
-//
-//        @Scheduled(fixedRate = 5000)
-//        public void outputMessage() {
-//            System.out.println("输出消息---------------------------------");
-//        }
-//
-//    }
+package com.heima.testBreath;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+    @Component
+    @EnableScheduling
+    public class MyScheduledTask {
+
+        @Scheduled(fixedRate = 60000)
+        public void outputMessage() {
+            System.out.println("我還活著---------------------------------");
+        }
+
+    }
